5066d7f8d6c4ee72fcc7e798c7e37988c9eeb3ea
Abdhesh <abdhesh.kumar@agamitechnologies.in>
2023-08-25 23:34:08 +0530

    adding the 25AUG2023 commit

---
+++ week_1/friday/first.cpp
@@ -0,0 +1,17 @@
+//preprocessor directive..
+//running program is called process.
+#include<iostream>
+//telling the dedicated namespace for cout...
+using namespace std;
+
+int main(){
+    /*
+    cout<<"Abdhesh Kumar"<<endl;
+    cout<<"Abdhesh Kumar"<<endl;
+    */
+    //cout is the identifier.
+    //Abdhesh kumar is a string
+    //; is for ending the line
+    cout<<"Hello World!"<<endl;
+    return 0;
+}
