5289500a6d2a9ba03c161bfb7e8db98f4494311b
Abdhesh <abdhesh.kumar@agamitechnologies.in>
2023-08-28 23:11:57 +0530

    done

---
+++ week_2/monday/pattern4_numericHalf.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i=5;
+    int j=5;
+
+    for(int row=0; row<i; row++){
+        for(int col=0; col<row+1;col++){
+            cout<<col+1;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
